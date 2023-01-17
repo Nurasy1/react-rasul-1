f26f71051542ce520b87117fdd7f7aa3eed0e3de
100ping <princenurik04@mail.ru>
2023-01-17 18:47:14 +0600

    Верстаем страницу Dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-const Dialogs =  (props) => {
+const Dialogs = (props) => {
 
 
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active }>
+                    Andrew
+                </div>
+                <div className={s.dialog}>
+                    Dmitry
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Rasul
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your university?</div>
+                <div className={s.dialog}>Yo</div>
+
+
+            </div>
         </div>
     )
 }
