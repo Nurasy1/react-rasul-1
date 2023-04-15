d406c06127008040426fd83d3dd12fb86c334cab
100ping <princenurik04@mail.ru>
2023-04-15 16:37:17 +0600

    ref . VirtualDom

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,8 +3,28 @@ import s from './../Dialogs.module.css';
 
 
 const Message = (props) => {
-    return <div className={s.dialogs}>{props.message}</div>
+
+    let newMessage = React.createRef();
+    let addMessage = () => {
+        let text = newMessage.current.value;
+        alert(text);
+    }
+
+    return (
+        <div className={s.dialogs}>{props.message}
+            <div>
+                <div>
+                <textarea ref={newMessage}></textarea>
+                </div>
+                <div>
+                    <button onClick={addMessage}>Send</button>
+                </div>
+            </div>
+        </div>
+
+    )
 }
+
 
 export default Message;
 
